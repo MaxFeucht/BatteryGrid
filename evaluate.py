05233a153f9a42dafd77f671949cca49cc7131f3
joris-hols-BB <joris.holshuijsen@brandboekhouders.nl>
2024-01-19 09:08:06 +0100

    Rule based agent update to percentile strategy

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import gymnasium as gym
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -39,11 +42,145 @@ class RuleEvaluation():
         self.presence = []
         self.balance = []
         self.actions = []
-                
-            
-    def rule_agent(self, obs, low_quantile, high_quantile):
+        self.model: LinearRegression
+                            
+    def simple_mean_reversal_strategy(self, obs, short_term, long_term):
+        print(f'prices: {obs["prices"][-short_term:]}')
+        short_sma = np.average(obs['prices'][max(0, len(obs['prices']) - short_term):])
+        long_sma = np.average(obs['prices'][max(0, len(obs['prices']) - long_term):])
+        #short_sma = np.average(obs['prices'][-short_term:])
+        print(f'short: {short_sma}')
+        #long_sma = np.average(obs['prices'][-long_term:])
+        print(f'long: {long_sma}')
+        if short_sma > long_sma:
+            action = 12
+        else:
+            action = 0
+        return action
+    
+    def train_model(self, train, val):
+        def get_season(month):
+            if month in [12, 1, 2]:
+                return 0  # Winter
+            elif month in [3, 4, 5]:
+                return 1  # Spring
+            elif month in [6, 7, 8]:
+                return 2  # Summer
+            elif month in [9, 10, 11]:
+                return 3  # Fall
+        # Get the data
+       #weekday = train['datetime'].dt.weekday
+
+        season = train['datetime'].dt.month.apply(get_season)
+
+        y = train['price']
+        #Normalize between 0 and 1
+        hours_normalized = (train['hour'] - 12)/24
+
+        #Can fill with zeroes or drop the first 7 rows from target and hours normalized
+        day_one = train['price'].shift(1).fillna(0)
+        day_two = train['price'].shift(2).fillna(0)
+        day_three = train['price'].shift(3).fillna(0)
+        day_four = train['price'].shift(4).fillna(0)
+        day_five = train['price'].shift(5).fillna(0)
+        day_six = train['price'].shift(6).fillna(0)
+        day_seven = train['price'].shift(7).fillna(0)
+
+        x = np.array([
+            hours_normalized,
+            day_one,
+            day_two,
+            day_three,
+            day_four,
+            day_five,
+            day_six,
+            day_seven]
+        ).reshape(-1, 8)
+    
+        #x = np.array([train['hour'], weekday, season]).reshape(-1, 3)
+        #Get validation data 
+        #season_val = val['datetime'].dt.month.apply(get_season)
+        #weekday_val = val['datetime'].dt.weekday
+        day_one_val = val['price'].shift(1).fillna(0)
+        day_two_val = val['price'].shift(2).fillna(0)
+        day_three_val = val['price'].shift(3).fillna(0)
+        day_four_val = val['price'].shift(4).fillna(0)
+        day_five_val = val['price'].shift(5).fillna(0)
+        day_six_val = val['price'].shift(6).fillna(0)
+        day_seven_val = val['price'].shift(7).fillna(0)
+       # x_val =  np.array([val['hour'], weekday_val, season_val]).reshape(-1, 3)
+        hours_val_normalized = (val['hour'] - 12)/24
+        x_val = np.array([
+            hours_val_normalized, 
+            day_one_val,
+            day_two_val,
+            day_three_val,
+            day_four_val,
+            day_five_val,
+            day_six_val,
+            day_seven_val
+                          ]).reshape(-1, 8)
+        y_val = val['price']
+
+        model = LinearRegression()
+        model.fit(x, y)
+        print("Coefficients: ", model.coef_)
+        print("Intercept: ", model.intercept_)
+
+        y_pred = model.predict(x_val)
+
+        mse = mean_squared_error(y_val, y_pred)
+        print("Mean squared error: ", mse)
+        r2 = r2_score(y_val, y_pred)
+        print("R2 score: ", r2)
+        self.model = model
+        return model
         
-        
+    
+    def regression_strategy(self, obs):
+        model = self.model
+        price_pred = model.predict(np.reshape(obs['hour'], (-1,1)))
+        # If the current price is smaller than predicted price we buy
+        if obs['prices'][-1] < price_pred:
+            #future_grad = price_pred - obs['prices'][-1]
+            #grad = np.gradient(obs['prices'])
+            #avg_grad_last_5_hours = np.mean(grad[-5:])      
+            action = 12
+        # If the current price is higher than predicted price we sell
+        elif obs['prices'][-1] > price_pred:
+            action = 0
+        else:
+            action = 6
+        return action
+    
+    def percentile_strategy(self, obs):
+        price = obs['prices'][-1]
+        price_history = obs['prices'][-self.env.price_horizon:]
+        percentiles = [np.quantile(price_history, q) for q in np.arange(0.1, 1.1, 0.1)]
+
+        if price < percentiles[0]:
+            action = 0
+        elif percentiles[0] <= price < percentiles[1]:
+            action = 2
+        elif percentiles[1] <= price < percentiles[2]:
+            action = 4
+        elif percentiles[3] <= price < percentiles[6]:
+            action = 6
+        elif percentiles[6] <= price < percentiles[7]:
+            action = 8
+        elif percentiles[7] <= price < percentiles[8]:
+            action = 10
+        elif price >= percentiles[9]:
+            action = 12
+        else:
+            action = np.random.randint(0, 12)
+
+        return action
+ 
+
+
+    def quantile_strategy(self, obs, low_quantile, high_quantile):
+
         if obs['prices'][-1] < np.quantile(obs['prices'][-self.env.price_horizon:], low_quantile):
             action = 0
         elif obs['prices'][-1] > np.quantile(obs['prices'][-self.env.price_horizon:], low_quantile) and obs['prices'][-1] < np.quantile(obs['prices'][-self.env.price_horizon:], high_quantile):
@@ -52,11 +189,24 @@ class RuleEvaluation():
             action = 12
         else:
             action = np.random.randint(0,12)
+
+
+        return action
             
+    def rule_agent(self, obs, func, **kwargs):
+        short_term = 30
+        long_term = 90
+        
+        action = func(obs=obs, **kwargs)
+        #action = self.quantile_strategy(obs, low_quantile, high_quantile)
+        #action = self.regression_strategy(obs)
+        #action = self.percentile_strategy(obs)
+    
+
         return action
 
 
-    def evaluate(self, low_quantile = 0.25, high_quantile = 0.75):
+    def evaluate(self, strategy, **kwargs):
         """
         Iterate through data and take actions based on price quantiles as a function of the price horizon
         """
@@ -64,7 +214,7 @@ class RuleEvaluation():
         self.data = self.df[self.env.price_horizon:] # Select data based on start index (price horizon)
         
         obs, info = self.env.reset() # Reset environment and get initial observation
-
+    
         # Iterate through data 
         for i, price in enumerate(self.data['price']):
             
@@ -72,7 +222,7 @@ class RuleEvaluation():
             self.battery_charge.append(obs['battery'])
             self.presence.append(obs['presence'])
             
-            action = self.rule_agent(obs, low_quantile, high_quantile) # Take action based on rule agent
+            action = self.rule_agent(obs, strategy, **kwargs) # Take action based on rule agent and strategy
             
             obs,r,t,info = self.env.step(action)
             self.actions.append(action)
